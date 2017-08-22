ad1a9d498bdc1ca86d8ce816a81157fb10bc2a72
arhh <aulee7@icloud.com>
2017-08-22 20:10:57 +0100

    Add House header for upload
    Header contains untested class declaration.

---
+++ house.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class House {
+    private:
+        int roomCount;
+        float length, breadth, height;
+        string description;
+    public:
+        House(int, int, int, int, string);
+        ~House();
+        int getRoomCount();
+        int getLength();
+        int getBreadth();
+        int getHeight();
+        string getDescription();
+        void setLength(int);
+        void setBreadth(int);
+        void setHeight(int);
+        void setDescription(string);
+}
