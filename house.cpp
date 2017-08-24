edd950d9a41f069239f31f9e396e8e8d273c3147
Arash H <arash@dx2300.localdomain>
2017-08-24 12:38:44 +0100

    Implement getRoomCount instance method

--- house.cpp
+++ house.cpp
@@ -13,12 +13,17 @@ House::~House() {
     cout << "Demolishing the house" << endl;
 }
 
+int House::getRoomCount() {
+    return roomCount;
+}
+
 int main() {
-    int sampleRoomCount = 1;
+    int sampleRoomCount = 2;
     int sampleLength = 12.8;
     int sampleBreadth = 8.5;
     int sampleHeight = 4.9;
-    string sampleDescription = "This house is small with one all-purpose room.";
+    string sampleDescription = "This house is small with two all-purpose rooms.";
     House thing(sampleRoomCount, sampleLength, sampleBreadth, sampleHeight, sampleDescription);
+    cout << thing.getRoomCount() << endl;
     return 0;
 }
