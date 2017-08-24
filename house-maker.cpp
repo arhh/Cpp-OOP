ce9047e7fb8759b1fe366ab4a43f06da4aa9aec9
arhh <aulee7@icloud.com>
2017-08-24 22:28:01 +0100

    Create pseudo-code for house-maker.cpp

---
+++ house-maker.cpp
@@ -0,0 +1,13 @@
+
+
+int main() {
+    // Display welcome message
+    
+    // While true
+    
+      // Call function to display menu with choices on what to do with house and assign result to variable
+    
+      // Switch statement for different results of prompt
+    return ;
+}
+
