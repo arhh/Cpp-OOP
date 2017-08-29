79b47f85b032f7801681c1e603e973c56f0fc3de
arhh <aulee7@icloud.com>
2017-08-29 22:47:40 +0100

    Complete initial implementation of setHouseDetails in house-maker.cpp

--- house-maker.cpp
+++ house-maker.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void makeHouse(House*);
 void getHouseDetails(House*);
+void setHouseDetails(House*);
 
 void makeHouse(House* emptyHouse) {
     int roomCount;
@@ -56,16 +57,86 @@ void getHouseDetails(House* house) {
                 cout << "Number of rooms in house: " << house -> getRoomCount() << endl;
                 break;
             case 2:
-                cout << "Length of house: " << house -> getLength() << " metres" << endl;
+                cout << "Length of house: " << house -> getLength() << " metre(s)" << endl;
                 break;
             case 3:
-                cout << "Breadth of house: " << house -> getBreadth() << " metres" << endl;
+                cout << "Breadth of house: " << house -> getBreadth() << " metre(s)" << endl;
                 break;
             case 4:
-                cout << "Height of house: " << house -> getHeight() << " metres" << endl;
+                cout << "Height of house: " << house -> getHeight() << " metre(s)" << endl;
                 break;
             case 5:
                 cout << "Description for house: " << house -> getDescription() << endl;
+                break;
+            default:
+                cout << "*** Invalid option selected ***" << endl;
+        }
+    cout << "\n***Returning to home screen***" << endl;
+}
+
+void setHouseDetails(House* house) {
+    int detailToSet;
+    
+    cout << "\n\nSpecify which of the following house details you want to modify:\n" << endl;
+    
+    cout << "1. Number of rooms" << endl;
+    cout << "2. Length" << endl;
+    cout << "3. Breadth" << endl;
+    cout << "4. Height" << endl;
+    cout << "5. Description" << endl;
+    cout << "> ";
+    
+    cin >> detailToSet;
+    
+    cout << "\nEnter new value:" << endl;
+    cout << "> ";
+    
+    switch(detailToSet) {
+            case 1:
+                {
+                int oldRoomCount, newRoomCount;
+                oldRoomCount = house -> getRoomCount();
+                cin >> newRoomCount;
+                house -> setRoomCount(newRoomCount);
+                cout << "Number of rooms in house updated from " << oldRoomCount << " to " << newRoomCount << endl;
+                }
+                break;
+            case 2:
+                {
+                float oldLength, newLength;
+                oldLength = house -> getLength();
+                cin >> newLength;
+                house -> setLength(newLength);
+                cout << "Length of house updated from " << oldLength << " to " << newLength << endl;
+                }
+                break;
+            case 3:
+                {
+                float oldBreadth, newBreadth;
+                oldBreadth = house -> getBreadth();
+                cin >> newBreadth;
+                house -> setBreadth(newBreadth);
+                cout << "Breadth of house updated from " << oldBreadth << " to " << newBreadth << endl;
+                }
+                break;
+            case 4:
+                {
+                float oldHeight, newHeight;
+                oldHeight = house -> getHeight();
+                cin >> newHeight;
+                house -> setHeight(newHeight);
+                cout << "Height of house updated from " << oldHeight << " to " << newHeight << endl;
+                }
+                break;
+            case 5:
+                {
+                string oldDescription, newDescription;
+                oldDescription = house -> getDescription();
+                cin.ignore(256, '\n');
+                getline(cin, newDescription);
+                house -> setDescription(newDescription);
+                cout << "Description of house updated from " << oldDescription << " to " << newDescription << endl;
+                }
                 break;
             default:
                 cout << "*** Invalid option selected ***" << endl;
@@ -96,11 +167,11 @@ int main() {
                 makeHouse(housePointer);
                 break;
             case 2:
-                cout << "You want to view the house's details" << endl;
                 getHouseDetails(housePointer);
                 break;
             case 3:
                 cout << "You want to modify the house's details" << endl;
+                setHouseDetails(housePointer);
                 break;
             case 4:
                 cout << "Closing program" << endl;
