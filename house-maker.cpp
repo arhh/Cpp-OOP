842f5d4afcc70452cf4d03b924aca17caca96156
arhh <aulee7@icloud.com>
2017-08-29 20:41:12 +0100

    Implement initial version of getHouseDetails in house-maker.cpp

--- house-maker.cpp
+++ house-maker.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void makeHouse(House*);
+void getHouseDetails(House*);
 
 void makeHouse(House* emptyHouse) {
     int roomCount;
@@ -14,11 +15,11 @@ void makeHouse(House* emptyHouse) {
     
     cout << "Number of rooms: ";
     cin >> roomCount;
-    cout << "Length: ";
+    cout << "Length (in metres): ";
     cin >> length;
-    cout << "Breadth: ";
+    cout << "Breadth (in metres): ";
     cin >> breadth;
-    cout << "Height: ";
+    cout << "Height (in metres): ";
     cin >> height;
     cout << "Description: ";
     cin.ignore(256, '\n');
@@ -34,6 +35,44 @@ void makeHouse(House* emptyHouse) {
     cout << "***Returning to home screen***" << endl;
 }
 
+void getHouseDetails(House* house) {
+    int detailToGet;
+    
+    cout << "\n\nSpecify which of the following house details you want to display:\n" << endl;
+    
+    cout << "1. Number of rooms" << endl;
+    cout << "2. Length" << endl;
+    cout << "3. Breadth" << endl;
+    cout << "4. Height" << endl;
+    cout << "5. Description" << endl;
+    cout << "> ";
+    
+    cin >> detailToGet;
+    
+    cout << endl;
+    
+    switch(detailToGet) {
+            case 1:
+                cout << "Number of rooms in house: " << house -> getRoomCount() << endl;
+                break;
+            case 2:
+                cout << "Length of house: " << house -> getLength() << " metres" << endl;
+                break;
+            case 3:
+                cout << "Breadth of house: " << house -> getBreadth() << " metres" << endl;
+                break;
+            case 4:
+                cout << "Height of house: " << house -> getHeight() << " metres" << endl;
+                break;
+            case 5:
+                cout << "Description for house: " << house -> getDescription() << endl;
+                break;
+            default:
+                cout << "*** Invalid option selected ***" << endl;
+        }
+    cout << "\n***Returning to home screen***" << endl;
+}
+
 int main() {
     int userIntendedTask;
     House house(0, 0, 0, 0, "");
@@ -44,7 +83,7 @@ int main() {
     do {
         cout << "\nWhat would you like to do:\n\n";
         cout << "1. Create a house\n";
-        cout << "2. View the detail of your house\n";
+        cout << "2. View some detail of your house\n";
         cout << "3. Change the details of your house\n";
         cout << "4. Close the program and destroy your house\n";
 
@@ -54,11 +93,11 @@ int main() {
 
         switch(userIntendedTask) {
             case 1:
-                cout << "You want to create a house" << endl;
                 makeHouse(housePointer);
                 break;
             case 2:
                 cout << "You want to view the house's details" << endl;
+                getHouseDetails(housePointer);
                 break;
             case 3:
                 cout << "You want to modify the house's details" << endl;
